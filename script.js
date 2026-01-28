3ef80652de50dee8076636f641d6be011928845f
nandu-afk <myname57197@gmail.com>
2026-01-28 19:31:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+let expenses = JSON.parse(localStorage.getItem("expenses")) || [];
 
+function addExpense() {
+  let amount = document.getElementById("amount").value;
+  let note = document.getElementById("note").value;
+
+  if (amount === "") {
+    alert("Amount likho");
+    return;
+  }
+
+  expenses.push({
+    amount: Number(amount),
+    note: note || "Kharcha"
+  });
+
+  localStorage.setItem("expenses", JSON.stringify(expenses));
+  document.getElementById("amount").value = "";
+  document.getElementById("note").value = "";
+  showExpenses();
+}
+
+function showExpenses() {
+  let list = document.getElementById("list");
+  let total = 0;
+  list.innerHTML = "";
+
+  expenses.forEach(e => {
+    total += e.amount;
+    let li = document.createElement("li");
+    li.textContent = `₹${e.amount} - ${e.note}`;
+    list.appendChild(li);
+  });
+
+  document.getElementById("total").textContent = "₹" + total;
+}
+
+showExpenses();
