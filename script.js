90f0932b57f3e00cddf78cd01c71b9ea16f82ee3
nandu-afk <myname57197@gmail.com>
2026-01-28 21:02:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 let expenses = JSON.parse(localStorage.getItem("expenses")) || [];
 
+function todayDate() {
+  return new Date().toISOString().split("T")[0];
+}
+
+document.getElementById("date").value = todayDate();
+
 function addExpense() {
   let amount = document.getElementById("amount").value;
   let note = document.getElementById("note").value;
+  let date = document.getElementById("date").value || todayDate();
 
   if (amount === "") {
     alert("Amount likho");
@@ -11,7 +18,8 @@ function addExpense() {
 
   expenses.push({
     amount: Number(amount),
-    note: note || "Kharcha"
+    note: note || "Kharcha",
+    date: date
   });
 
   saveAndRefresh();
@@ -26,6 +34,7 @@ function saveAndRefresh() {
   localStorage.setItem("expenses", JSON.stringify(expenses));
   document.getElementById("amount").value = "";
   document.getElementById("note").value = "";
+  document.getElementById("date").value = todayDate();
   showExpenses();
 }
 
@@ -34,16 +43,19 @@ function showExpenses() {
   let total = 0;
   list.innerHTML = "";
 
+  let today = todayDate();
+
   expenses.forEach((e, index) => {
-    total += e.amount;
+    if (e.date === today) {
+      total += e.amount;
 
-    let li = document.createElement("li");
-    li.innerHTML = `
-      ₹${e.amount} - ${e.note}
-      <button onclick="deleteExpense(${index})">❌</button>
-    `;
-
-    list.appendChild(li);
+      let li = document.createElement("li");
+      li.innerHTML = `
+        ₹${e.amount} - ${e.note}
+        <button onclick="deleteExpense(${index})">❌</button>
+      `;
+      list.appendChild(li);
+    }
   });
 
   document.getElementById("total").textContent = "₹" + total;
